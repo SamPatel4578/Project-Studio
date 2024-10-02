33567d5c817c631e1d9737a103c7aa91830905e3
Saumya Patel <patsy076@mymail.unisa.edu.au>
2024-10-02 14:32:07 +0930

    Updated the UI/UX for some webpages
    Updated the UI/UX for some webpages

--- UTR WebApplication/obj/Debug/net8.0/UTR WebApplication.AssemblyInfo.cs
+++ UTR WebApplication/obj/Debug/net8.0/UTR WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTR WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4069dd285fce1bd261b19abbb1e59443bfc68bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405903f72a7eca2b337f6406cd8d97bbd6b9c153")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTR WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTR WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
